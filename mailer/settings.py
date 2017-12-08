90d0f54b18a70bb49757514afd1669a2d77f13bb
BlisS <contacto@fixter.org>
2017-12-08 16:21:49 -0600

    static root

--- mailer/settings.py
+++ mailer/settings.py
@@ -128,7 +128,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 # Correo electronico
 EMAIL_HOST='smtp.gmail.com'
 EMAIL_PORT = '587'
