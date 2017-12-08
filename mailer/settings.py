fa54f9d3e40de69e9f1d8f2964e8e6b121da20b1
BlisS <contacto@fixter.org>
2017-12-08 16:39:13 -0600

    client

--- mailer/settings.py
+++ mailer/settings.py
@@ -76,18 +76,18 @@ WSGI_APPLICATION = 'mailer.wsgi.application'
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #     'NAME': 'mailer',
-    #     'USER':'bliss',
-    #     'PASSWORD': 'password',
-    #     'HOST':'localhost'
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     # }
+
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'mailer',
+        'USER':'bliss',
+        'PASSWORD': 'password',
+        'HOST':'localhost'
+    }
 }
 
 
