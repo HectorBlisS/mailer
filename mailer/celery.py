e90aaf923bf1b4a8175d199489f0ae6e416bbb3a
BlisS <contacto@fixter.org>
2017-12-08 18:42:55 -0600

    testing

--- mailer/celery.py
+++ mailer/celery.py
@@ -11,5 +11,8 @@ app.config_from_object("django.conf:settings")
 app.autodiscover_tasks(lambda:settings.INSTALLED_APPS)
 
 # heroku config
-# app.conf.update(BROKER_URL=os.environ['REDIS_URL'], CELERY_TASK_SERIALIZER="json")
+app.conf.update(CELERY_BROKER_URL = os.environ['REDIS_URL'],
+CELERY_ACCEPT_CONTENT = ['json'],
+CELERY_TASK_SERIALIZER = 'json',
+CELERY_RESULT_SERIALIZER = 'json')
 
