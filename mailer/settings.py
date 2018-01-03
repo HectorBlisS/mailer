484e9541f33067679a100631ab193e9fc4101e34
BlisS <contacto@fixter.org>
2018-01-03 15:55:24 -0600

    heroku

--- mailer/settings.py
+++ mailer/settings.py
@@ -132,9 +132,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),) 
-# STATIC_ROOT = os.path.join(BASE_DIR, "static")
-# STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+#STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),) 
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Correo electronico
 EMAIL_HOST='smtp.gmail.com'
