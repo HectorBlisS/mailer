b0fb22b7fc1e4801d8557a64c86582fd9814acf4
BlisS <contacto@fixter.org>
2018-01-04 09:52:23 -0600

    two config celery heroku

--- mailer/celery.py
+++ mailer/celery.py
@@ -11,5 +11,5 @@ app = Celery("mailer")
 app.config_from_object("django.conf:settings")
 app.autodiscover_tasks(lambda:settings.INSTALLED_APPS)
 
-app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
-                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
+# app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+#                 CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
